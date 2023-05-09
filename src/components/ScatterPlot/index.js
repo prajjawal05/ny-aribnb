5d1e956097f15978222f0ba765f9a9edb6e95499
prajjawal05 <prajjawalagarwal@gmail.com>
2023-05-09 02:25:14 -0400

    change axes in scatterplot and modified scatterdata for yearRange

--- src/components/ScatterPlot/index.js
+++ src/components/ScatterPlot/index.js
@@ -24,38 +24,35 @@ var brushed = ({ selection }) => {
 var renderPlot = () => {
     const svg = d3.select('#scatterplot');
     var margin = { top: 70, bottom: 70, left: 70, right: 70 };
-    // 350 is graph width and height
+
     const brush = d3.brush()
         .extent([[margin.left, margin.top], [margin.left + 350, margin.top + 350]])
         .on("start brush end", brushed);
 
     svg.call(brush);
 
-    var selectedVariableX = "number of reviews";
+    var selectedVariableX = "review rate number";
     var selectedVariableY = "price";
-    var scatterPlotData = [];
 
-    var minReviews = 0;
-    var maxReviews = 0;
     var cancellationPolicyMap = { "strict": 4, "moderate": 3, "flexible": 2 };
-    data.forEach(row => {
-        minReviews = Math.min(minReviews, row["number of reviews"]);
-        maxReviews = Math.max(maxReviews, row["number of reviews"]);
-        let tempData = { "xLabelValue": row[selectedVariableX], "yLabelValue": row[selectedVariableY], "review": row["number of reviews"], "cancellation_policy": cancellationPolicyMap[row["cancellation_policy"]] };
-        scatterPlotData.push(tempData);
-    });
 
-    console.log(minReviews + " and " + maxReviews + " ");
+    const scatterPlotData = data.map(row => ({
+        "xLabelValue": row[selectedVariableX],
+        "yLabelValue": row[selectedVariableY],
+        "review": row["number of reviews"],
+        "numReviews": row["review rate number"],
+        "cancellation_policy": cancellationPolicyMap[row["cancellation_policy"]]
+    }));
 
     var xScale = d3.scaleLinear()
         .range([0, 350])
-        .domain([
-            d3.min(scatterPlotData, d => d.xLabelValue),
-            d3.max(scatterPlotData, d => d.xLabelValue)
-        ])
+        .domain([0, 5])
         .nice();
 
-    var yScale = d3.scaleLinear().range([350, 0]).domain([d3.min(scatterPlotData, function (d) { return d.yLabelValue; }), d3.max(scatterPlotData, function (d) { return d.yLabelValue; })]).nice();
+    var yScale = d3.scaleLinear().range([350, 0]).domain([
+        d3.min(scatterPlotData, function (d) { return d.yLabelValue; }),
+        d3.max(scatterPlotData, function (d) { return d.yLabelValue; })]
+    ).nice();
 
     var xAxis = svg.append("g")
         .classed("xAxis", true)
@@ -71,32 +68,38 @@ var renderPlot = () => {
     var funYAxis = d3.axisLeft(yScale);
     funYAxis(yAxis);
 
-    // graph.append("text")
-    // .attr("class", "x-axis-label")
-    // .attr("text-anchor", "end")
-    // .attr("x", width / 2.5)
-    // .attr("y", height - margin.bottom + 15)
-    // .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-    // .attr("font-weight", "bold")
-    // .text(selectedVariableX.name);
+    // svg.append("text")
+    //     .attr("class", "x-axis-label")
+    //     .attr("text-anchor", "end")
+    //     // .attr("x", 700 / 2.5)
+    //     // .attr("y", 400 - margin.bottom + 15)
+    //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    //     .attr("font-weight", "bold")
+    //     .style("color", "white")
+    //     .text(selectedVariableX);
 
-    // graph.append("text")
-    // .attr("class", "y-axis-label")
-    // .attr("text-anchor", "end")
-    // .attr("transform", "rotate(-90)")
-    // .attr("y", -70)
-    // .attr("font-weight", "bold")
-    // .text(selectedVariableY.name);
+    // svg.append("text")
+    //     .attr("class", "y-axis-label")
+    //     .attr("text-anchor", "end")
+    //     .attr("transform", "rotate(-90)")
+    //     .attr("y", -10)
+    //     .attr("font-weight", "bold")
+    //     .style("color", "white")
+    //     .text(selectedVariableY);
 
-    var pointGroup = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var pointGroup = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    pointGroup.selectAll("circle").data(scatterPlotData).enter()
+    pointGroup.selectAll("circle")
+        .data(scatterPlotData)
+        .enter()
         .append("circle")
         .attr("cx", data => xScale(data.xLabelValue))
         .attr("cy", data => yScale(data.yLabelValue))
         .attr("r", data => {
-            return 15 * (data.review - minReviews) / (maxReviews - minReviews);
+            return data.review / 20;
         })
+        .style("opacity", 0.5)
         .attr("fill", data => getColorScale()(data.cancellation_policy));
 };
 
