8313f85f90fb8dc7b8dd45f0a34cc6cf2a19a6ad
prajjawal05 <prajjawalagarwal@gmail.com>
2023-05-10 20:00:14 -0400

    some cosmetic changes in bar graph

--- src/components/BarGraph/index.js
+++ src/components/BarGraph/index.js
@@ -2,69 +2,31 @@ import data from "../ScatterPlot/scatterdata.json";
 import * as d3 from "d3";
 import { useEffect } from "react";
 
-const BarGraph = () => {
-    useEffect(() => {
-        renderGraph();
-    });
+import StyledSVG from "./style";
 
-    return (
-        <svg id="bar-graph" width="600" height="600" />
-    );
-};
 
-var createList = (data, selectedVariable) => {
-    var freqMap = [];
-    var ranges = ["2003-07", "2008-12", "2013-17", "2018-22"]
-    var yearRange = {
-        "2003-07": { "Entire_home_apt": 0, "Hotel_room": 0, "Private_room": 0, "Shared_room": 0 },
-        "2008-12": { "Entire_home_apt": 0, "Hotel_room": 0, "Private_room": 0, "Shared_room": 0 },
-        "2013-17": { "Entire_home_apt": 0, "Hotel_room": 0, "Private_room": 0, "Shared_room": 0 },
-        "2018-22": { "Entire_home_apt": 0, "Hotel_room": 0, "Private_room": 0, "Shared_room": 0 }
-    };
-    data.forEach(row => {
-        if (row['room type'] === "Hotel room") {
-            yearRange[row[selectedVariable.name]].Hotel_room++;
-        }
-        else if (row['room type'] === "Private room") {
-            yearRange[row[selectedVariable.name]].Private_room++;
-        }
-        else if (row['room type'] === "Shared room") {
-            yearRange[row[selectedVariable.name]].Shared_room++;
-        }
-        else {
-            yearRange[row[selectedVariable.name]].Entire_home_apt++;
-        }
-    });
-    // console.log(yearRange);
-    for (let range of ranges) {
-        freqMap.push({ "key": range, "Entire_home_apt": yearRange[range].Entire_home_apt, "Hotel_room": yearRange[range].Hotel_room, "Private_room": yearRange[range].Private_room, "Shared_room": yearRange[range].Shared_room })
-    }
-    // console.log(freqMap);
-    return freqMap
-}
+const margin = { top: 70, bottom: 70, left: 70, right: 70 };
+const width = 500, height = 500;
 
-var renderGraph = () => {
+const xLabel = "Year Range";
+const yLabel = "Frequency";
+const xScale = d3.scaleBand().range([0, width]).padding(0.2);
+const yScale = d3.scaleLinear().range([height, 0]).nice();
 
-    const svg = d3.select('#bar-graph');
+const makeAxes = (svg) => {
+    svg.append("g")
+        .classed("xAxis", true)
+        .style("color", "white")
+        .attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
+        .call(d3.axisBottom(xScale));
+
+    svg.append("g")
+        .classed("yAxis", true)
+        .style("color", "white")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .call(d3.axisLeft(yScale))
+
     const graph = svg.append("g").attr("transform", "translate(" + 100 + "," + 100 + ")");
-    const margin = { top: 70, bottom: 70, left: 70, right: 70 };
-    const width = 500, height = 500;
-    const selectedVariable = { "name": "year_range" };
-    const graphData = createList(data, selectedVariable);
-
-
-    const xLabel = selectedVariable.name;
-    const yLabel = "Frequency";
-    const xScale = d3.scaleBand().range([0, width]).padding(0.2);
-    const yScale = d3.scaleLinear().range([height, 0]).nice();
-    xScale.domain(graphData.map(function (d) { return d.key; }));
-    yScale.domain([0, 700]);
-
-    var xAxis = svg.append("g").classed("xAxis", true).style("color", "white").attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")").call(d3.axisBottom(xScale));
-    var yAxis = svg.append("g").classed("yAxis", true).style("color", "white").attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-
-    var funYAxis = d3.axisLeft(yScale);
-    funYAxis(yAxis);
 
     graph.append("text")
         .attr("class", "x-axis-label")
@@ -84,11 +46,59 @@ var renderGraph = () => {
         .attr("y", -70)
         .attr("font-weight", "bold")
         .text(yLabel);
+};
 
-    const stack = d3.stack()
-        .keys(["Private_room", "Entire_home_apt", "Hotel_room", "Shared_room"]);
-    const stackedData = stack(graphData);
-    // console.log(stackedData);
+var createList = (data, selectedVariable) => {
+    var freqMap = [];
+    var ranges = ["2003-07", "2008-12", "2013-17", "2018-22"]
+    var yearRange = {
+        "2003-07": { "Entire_home_apt": 0, "Hotel_room": 0, "Private_room": 0, "Shared_room": 0 },
+        "2008-12": { "Entire_home_apt": 0, "Hotel_room": 0, "Private_room": 0, "Shared_room": 0 },
+        "2013-17": { "Entire_home_apt": 0, "Hotel_room": 0, "Private_room": 0, "Shared_room": 0 },
+        "2018-22": { "Entire_home_apt": 0, "Hotel_room": 0, "Private_room": 0, "Shared_room": 0 }
+    };
+    data.forEach(row => {
+        if (row['room type'] === "Hotel room") {
+            yearRange[row[selectedVariable]].Hotel_room++;
+        }
+        else if (row['room type'] === "Private room") {
+            yearRange[row[selectedVariable]].Private_room++;
+        }
+        else if (row['room type'] === "Shared room") {
+            yearRange[row[selectedVariable]].Shared_room++;
+        }
+        else {
+            yearRange[row[selectedVariable]].Entire_home_apt++;
+        }
+    });
+    for (let range of ranges) {
+        freqMap.push({ "key": range, "Entire_home_apt": yearRange[range].Entire_home_apt, "Hotel_room": yearRange[range].Hotel_room, "Private_room": yearRange[range].Private_room, "Shared_room": yearRange[range].Shared_room })
+    }
+    return freqMap
+}
+
+var renderGraph = () => {
+    const svg = d3.select('#bar-graph');
+    const attribute = "year_range";
+    const graphData = createList(data, attribute);
+
+    xScale.domain(graphData.map(function (d) { return d.key; }));
+    yScale.domain([0, d3.max(graphData, function (d) { return d.Entire_home_apt + d.Hotel_room + d.Shared_room + d.Private_room; })]);
+    makeAxes(svg);
+
+    const stackedData = d3.stack()
+        .keys(["Private_room", "Entire_home_apt", "Hotel_room", "Shared_room"])(graphData)
+        .map(roomWise => {
+            let newData = roomWise.map(({ data, ...rest }) => ({
+                ...rest,
+                data: {
+                    ...data,
+                    roomType: roomWise.key
+                }
+            }))
+            newData.key = roomWise.key;
+            return newData;
+        });
 
     const barGroup = svg.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -97,20 +107,41 @@ var renderGraph = () => {
         .domain(["Private_room", "Entire_home_apt", "Hotel_room", "Shared_room"])
         .range([d3.interpolateReds(1), d3.interpolateReds(0.75), d3.interpolateReds(0.5), d3.interpolateReds(0.25)]);
 
+    const barWidth = xScale.bandwidth();
     barGroup.selectAll("g")
         .data(stackedData)
         .enter().append("g")
-        .attr("fill", d => { console.log(d.key, color(d.key)); return color(d.key); })
+        .attr("fill", d => { return color(d.key); })
         .selectAll("rect")
         .data(d => { return d; })
         .enter().append("rect")
         .attr("x", d => { return xScale(d.data.key); })
         .attr("y", d => { return yScale(d[1]); })
-        .attr("width", xScale.bandwidth())
-        .attr("height", d => { return yScale(d[0]) - yScale(d[1]); })
-        .on("mouseover", (e, d) => {
-            console.log(e, d);
+        .attr("width", barWidth)
+        .attr("height", d => { return (yScale(d[0]) - yScale(d[1])) - 2; })
+        .on("mouseover", function (e, d) {
+            d3.select(this)
+                .classed('highlighted', true)
+                .attr("width", barWidth * 1.25)
+                .attr("x", d => { return xScale(d.data.key) - barWidth / 8; });
+        }).on("mouseout", function (e, d) {
+            d3.select(this)
+                .classed('highlighted', false)
+                .attr("width", barWidth)
+                .attr("x", d => { return xScale(d.data.key); });
         });
+
+
+};
+
+const BarGraph = () => {
+    useEffect(() => {
+        renderGraph();
+    }, []);
+
+    return (
+        <StyledSVG id="bar-graph" width="600" height="600" />
+    );
 };
 
 export default BarGraph;
---
+++ src/components/BarGraph/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const StyledSVG = styled.svg`
+    .highlighted {
+        stroke: black;
+        stroke-width: 2;
+        cursor: pointer;
+    }
+`;
+
+export default StyledSVG;
